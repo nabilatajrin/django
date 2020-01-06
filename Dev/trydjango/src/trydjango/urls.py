22db203611a86bdc0c7bcb70e4b40b8ee913f63e
nabila <nabilatajrin@gmail.com>
2020-01-07 00:09:36 +0600

    12:10AM 07JAN2019 update

--- Dev/trydjango/src/trydjango/urls.py
+++ Dev/trydjango/src/trydjango/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from pages.views import home_view, contact_view, about_view
+
 
 urlpatterns = [
+    #path('blog/', include('blog.urls')),
+    #path('courses/', include('courses.urls')),
+    #path('products/', include('products.urls')),
+    path('home/', home_view, name='home'),
+    path('', home_view, name='home'),
+    path('about/<int:id>/', about_view, name='product-detail'),
+    path('contact/', contact_view),
     path('admin/', admin.site.urls),
 ]
+
+
