93204dc7ce45508384a66225f1c83d44fa672a1f
nabila <nabilatajrin@gmail.com>
2020-01-07 11:49:07 +0600

    11:48AM 07JAN2019 for loop in a template

--- Dev/trydjango/src/pages/views.py
+++ Dev/trydjango/src/pages/views.py
@@ -16,8 +16,9 @@ def about_view(request, *args, **kwargs):
     my_context = {
         "my_text": "abc this is about us",
         "my_number": 123,
-        "my_list": [123, 4345, 555]
+        "my_list": [123, 4345, 55565, "Abc"]
     }
+
     return render(request, "about.html", my_context)
 
 
