fd4f57beab279904f08714ac7a29110518cf4ebd
nabila <nabilatajrin@gmail.com>
2020-01-07 15:39:25 +0600

    03:39PM 07JAN2019 render data from the database

--- Dev/trydjango/src/products/views.py
+++ Dev/trydjango/src/products/views.py
@@ -4,11 +4,8 @@ from .models import Product
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
-    context = {
-        'title': obj.title,
-        'description': obj.description
-    }
+
     context = {
         'object:': obj
     }
-    return render(request, "product/detail.html", context)
+    return render(request, "products/product_detail.html", context)
