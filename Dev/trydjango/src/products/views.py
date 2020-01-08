8c3997b13526f373e5d76f1b58116e94d95b81fa
nabila <nabilatajrin@gmail.com>
2020-01-08 10:49:47 +0600

    10:50AM 08JAN2020 update

--- Dev/trydjango/src/products/views.py
+++ Dev/trydjango/src/products/views.py
@@ -5,16 +5,12 @@ from .models import Product
 # Create your views here.
 
 def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form.ProductForm()
     context = {
-        "form": my_form
+        "form": form
     }
     return render(request, "products/product_create.html", context)
 
